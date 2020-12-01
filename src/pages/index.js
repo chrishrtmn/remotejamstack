8e0e9a0f4b3c83afc5086cecf28b5579222e9896
Chris Hartman <chrishartman@me.com>
2020-12-01 13:24:08 -0500

    Removing vercel json, updating content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 
+const client = require('contentful').createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+})
+
+async function fetchJobPosts() {
+  const entries = await client.getEntries()
+  if (entries.items) return entries.items.map(({ fields }) => fields)
+  console.log(`Error getting Entries for ${contentType.name}.`)
+}
+
 export default function Home() {
+  const [jobPosts, setJobPosts] = useState([])
+
+  useEffect(() => {
+    async function getJobPosts() {
+      const posts = await fetchJobPosts()
+      setJobPosts([...posts])
+    }
+
+    getJobPosts()
+  }, [])
+
   return (
     <div>
       <Head>
@@ -8,9 +31,22 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1>Hello world</h1>
-      </main>
+      <div>
+        <h1>My Job Board</h1>
+
+        {jobPosts.length > 0
+          ? jobPosts.map((job) => (
+              <div>
+                <img src={job.companyImage} />
+                <a href={job.link}>
+                  {job.title} - {job.company}
+                </a>
+                <p>{job.location}</p>
+                <p>{job.createdAt}</p>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   )
 }
