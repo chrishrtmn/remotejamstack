c8474588b63e7d430bb0729089f97cad8a8c195d
Chris Hartman <chrishartman@me.com>
2020-12-02 17:31:35 -0500

    Adding custom tailwind stylesheets

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import '../styles/base.css'
+import '../styles/components.css'
+import '../styles/custom.css'
+import '../styles/utilities.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
