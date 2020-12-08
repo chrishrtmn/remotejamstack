2b22aeaff0c937117302a1886ae56ac08c5f6cb5
Chris Hartman <chrishartman@me.com>
2020-12-08 00:47:08 -0500

    General updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
 import Head from 'next/head'
+//import Subscribe from '../components/subscribe'
 
 export default function Home() {
   return (
@@ -12,11 +12,11 @@ export default function Home() {
         <div className='text-2xl text-white'>
           Remote <span className='font-bold'>Jamstack</span>
         </div>
+
         <h1 className='max-w-md py-20 mx-auto text-5xl font-medium text-center text-white'>
           <span className='text-theme-red'>Remote</span> jobs for the{' '}
           <span className='pt-2 text-theme-red'>Jamstack</span> ecosystem
         </h1>
-        <p className='text-lg text-white'>Coming soon.</p>
       </div>
     </div>
   )
