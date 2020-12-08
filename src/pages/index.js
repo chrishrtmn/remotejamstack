426b7181ddc810d2085e1b9be69bf76cd996b3c3
Chris Hartman <chrishartman@me.com>
2020-12-07 22:03:35 -0500

    Adding pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,7 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 
-const client = require('contentful').createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-})
-
-async function fetchJobPosts() {
-  const entries = await client.getEntries()
-  if (entries.items) return entries.items.map(({ fields }) => fields)
-  console.log(`Error getting Entries for ${contentType.name}.`)
-}
-
 export default function Home() {
-  const [jobPosts, setJobPosts] = useState([])
-
-  useEffect(() => {
-    async function getJobPosts() {
-      const posts = await fetchJobPosts()
-      setJobPosts([...posts])
-    }
-
-    getJobPosts()
-  }, [])
-
   return (
     <div>
       <Head>
@@ -34,32 +12,11 @@ export default function Home() {
         <div className='text-2xl text-white'>
           Remote <span className='font-bold'>Jamstack</span>
         </div>
-        <h1 className='py-20 text-5xl font-medium text-white'>
-          Remote jobs for the{' '}
-          <span className='block pt-2 text-theme-red'>Jamstack ecosystem</span>
+        <h1 className='max-w-md py-20 mx-auto text-5xl font-medium text-center text-white'>
+          <span className='text-theme-red'>Remote</span> jobs for the{' '}
+          <span className='pt-2 text-theme-red'>Jamstack</span> ecosystem
         </h1>
         <p className='text-lg text-white'>Coming soon.</p>
-        {/*<nav>
-          <ul className='text-white'>
-            <li>Jobs</li>
-            <li>Employers</li>
-            <li className='px-4 py-2 bg-theme-blue'>Post a Job</li>
-          </ul>
-        </nav>*/}
-      </div>
-      <div>
-        {/**jobPosts.length > 0
-          ? jobPosts.map((job) => (
-              <div>
-                <img src={job.companyImage} />
-                <a href={job.link}>
-                  {job.title} - {job.company}
-                </a>
-                <p>{job.location}</p>
-                <p>{job.createdAt}</p>
-              </div>
-            ))
-          : null**/}
       </div>
     </div>
   )
