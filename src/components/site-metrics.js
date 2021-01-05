a0b463a52ecc52a061dafecb8965b1cc858cea99
Chris Hartman <chrishartman@me.com>
2021-01-04 23:44:09 -0500

    Updating metrics

--- src/components/site-metrics.js
+++ src/components/site-metrics.js
@@ -58,7 +58,7 @@ const SiteMetrics = () => (
               </svg>
             </div>
             <div className='text-4xl font-bold text-gray-200 group-hover:text-white'>
-              7
+              10
               <svg
                 className='inline-block w-5 h-5'
                 xmlns='http://www.w3.org/2000/svg'
